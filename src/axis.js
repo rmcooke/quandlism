068e5ee9b0858290b9f639669ed9a2ba8aaa7a04
Jason Byck <jasonbyck@gmail.com>
2012-10-24 16:53:37 -0400

    Refactor example. Update axis...better but not good

--- src/axis.js
+++ src/axis.js
@@ -2,18 +2,23 @@ QuandlismContext_.axis = function() {
   var context = this,
   scale = d3.time.scale().domain([0, length]).range([0, context.width()]),
   axis_ = d3.svg.axis().scale(scale),
+  active = false,
   data;
   
   function axis(selection) {
     var tick;
-    
+        
     data = selection.datum();
     
-    start = data[0].values[0].date;
-    endin = data[0].values[data[0].values.length - 1].date;
-    scale.domain([new Date(2010, 01, 01), new Date(2012, 10, 01)]);
-    axis_.ticks(6)
+    extent = [d3.first(data), d3.last(data)];
+    
+    parseDate = context.utility().parseDate();
+        
+    scale.domain([parseDate(extent[0]), parseDate(extent[1])]);
+   
     axis_.tickFormat(d3.time.format('%b %d, %Y'));
+   
+    axis_.ticks(6, 2, 2);
         
     var g = selection.append('svg')
         .attr('width', context.width())
@@ -21,6 +26,14 @@ QuandlismContext_.axis = function() {
       .append('g')
         .attr('transform', 'translate(0,27)')
         .call(axis_);
+  }
+  
+  axis.active = function(_) {
+    if (!arguments.length) {
+      return active;
+    }
+    active = _;
+    return axis;
   }
   
   return d3.rebind(axis, axis_, 'orient', 'ticks', 'ticksSubdivide', 'tickSize', 'tickPadding', 'tickFormat');
