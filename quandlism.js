761655f4d225c44eecc59a20d195aa93e7306e34
Jason Byck <jasonbyck@gmail.com>
2012-11-01 14:17:32 -0400

    Update widget sizes

--- quandlism.js
+++ quandlism.js
@@ -134,9 +134,9 @@ var QuandlismContext_ = QuandlismContext.prototype = quandlism.context.prototype
 
 var quandlism_axis  = 0;
 var quandlism_line_id = 0;
-var quandlism_stage = {w: 0.90, h: 0.65};
+var quandlism_stage = {w: 0.90, h: 0.55};
 var quandlism_brush = {w: 0.90, h: 0.15};
-var quandlism_xaxis = {w: 0.90, h: 0.2};
+var quandlism_xaxis = {w: 0.90, h: 0.15};
 var quandlism_yaxis = {w: 0.10, h: 0.7};
 /**
  * Quandlism Line
