a980d7a5144fe75ae0f8f4db59e4100886cd26da
Jason Byck <jasonbyck@gmail.com>
2013-01-16 13:03:22 -0500

    Increment to 0.6.1

--- quandlism.js
+++ quandlism.js
@@ -4,7 +4,7 @@
     __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
   quandlism = exports.quandlism = {
-    version: '0.6.0'
+    version: '0.6.1'
   };
 
   quandlism.context = function() {
